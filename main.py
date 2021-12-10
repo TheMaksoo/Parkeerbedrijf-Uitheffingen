d1db225581795fb4c07e05b62d752ebfd702d6a4
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2021-12-10 10:47:55 +0100

    .

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ dataFile = open("PO.csv", "r")
 dataDict = csv.DictReader(dataFile)
 dataList = list(dataDict)
 
+acties = 0
+ontheffingen = 0
+gemiddelde = 0
+
 os.system('cls')
 print(f"Welkom bij de parkeer uiheffingen.!")
-os.system('cls')
+
+for ontheffing in dataList:
+    acties += int(ontheffing["acties"])
+    ontheffingen += 1
+
+gemiddelde += acties / ontheffingen
+
+print(f"Er zijn gemiddeled {gemiddelde} gemiddelde acties per ontheffing.")
